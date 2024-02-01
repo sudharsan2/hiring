4ae44253c6b6e5847a9b110ab6e6184f8c093459
sudharsan <sudharsanselvam2@gmail.com>
2024-02-01 11:57:55 +0530

    first_commit

--- HiringBackend/settings.py
+++ HiringBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*aofefpe+p1ccq5r(&q_f52in_2pu)uuehjbo74=m)0$kmmt1l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['172.235.10.116']
 
 
 # Application definition
