8fc16c7d2fc7b06899c7748fbc45406482507409
sudharsan <sudharsanselvam2@gmail.com>
2024-02-01 16:05:24 +0530

    first commit

--- userManagement/urls.py
+++ userManagement/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import RegistrationView, LoginView, GetAllUsersView, GetAllRolesView
+from .views import RegistrationView, LoginView, GetAllUsersView, GetAllRolesView,add_user_roles
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('signin/', LoginView.as_view(), name='login'),
     path('getAllUsers/', GetAllUsersView.as_view(), name='getAllUsers'),
     path('getAllRoles/', GetAllRolesView.as_view(), name='getAllRoles'),
+    path('addUserRoles/',add_user_roles.as_view(), name = 'adduseroles')
 ]
